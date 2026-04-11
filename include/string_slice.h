a03f9e410d14e49e5440b86384f8705667225bd6
Nadelio <carefulsniffle@gmail.com>
2026-04-11 15:28:47 -0500

    Working on refactor StringSlice -> Slice, Buffer

--- include/string_slice.h
+++ include/string_slice.h
@@ -17,28 +17,79 @@ typedef int64_t  i64;
 
 /// @brief A C string with an enforced length
 typedef struct {
-    uint64_t len;
+    size_t len;
     char* begin;
-} StringSlice;
+} Slice;
+
+// @brief A string that can be used to concatenate to
+typede struct {
+	size_t used_capacity;
+	size_t total_capacity;
+	char* buffer;
+} Buffer;
+
+/// @brief Zeros a portion of memory starting a destination and ending at destination + count
+/// @param destination The pointer to begin writing zeros at
+/// @param count The amount of zeros to write
+void* memzero(void* destination, size_t count);
 
 // Function declarations
 /// @brief Converts a slice back into a standard C string
-/// @param slice The source StringSlice
-char* slice_to_string(const StringSlice slice);
+/// @param slice The source Slice
+char* slice_to_string(const Slice slice);
 
-/// @brief Converts a string to a StringSlice
+/// @brief Converts a string to a Slice
 /// @param string The source string
-StringSlice string_to_slice(const char* string);
+Slice* string_to_slice(const char* string);
 
-/// @brief Creates a StringSlice given a beginning offset and a total length
+/// @brief Creates a Slice given a beginning offset and a total length
 /// @param string The source string
 /// @param offset The offset of the start of the slice
 /// @param length The total length of the slice
-StringSlice slice_from_range(char* string, u64 offset, u64 length);
+Slice* slice_from_range(char* string, size_t offset, size_t length);
 
-/// @brief Compares two string slices together and checks if they are equivalent or not
+/// @brief Compares two slices together and checks if they are equivalent or not
 /// @param str_1 The first slice
 /// @param str_2 The second slice
-bool streql(const StringSlice* str_1, const StringSlice* str_2);
+bool streql(const Slice* str_1, const Slice* str_2);
+
+/// @brief Add the contents of a slice to the end of a buffer and return that buffer 
+/// @param buffer The buffer to concatenate slice to
+/// @param slice The slice to concatenate at the end of buffer
+Buffer* concat(Buffer* buffer, Slice* slice);
+
+/// @brief Add a C string to the end of a buffer and return that buffer
+/// @param buffer The buffer to concatenate the C string to
+/// @param cstr The C string to concatenate at the end of buffer
+Buffer* concat_cstr(Buffer* buffer, char* cstr);
+
+/// @brief Find the first occurrence of a specific character in a Slice and return the index of that character
+/// @param slice The slice to search for the character in
+/// @param search_char The character to search for in the slice
+size_t find_first(Slice* slice, char search_char);
+
+/// @brief Find the first occurrence of a specific character in a Buffer and return the index of that character
+/// @param buffer The buffer to search for the character in
+/// @param search_char The character to search for in the buffer
+size_t find_first_in_buffer(Buffer* buffer, char search_char);
+
+/// @brief Compare to slices together and return a 1 if s1 > s2, return 0 if s1 == s2, and return -1 if s1 < s2
+/// @param s1 The first slice
+/// @param s2 The second slice
+i8 slice_cmp(Slice* s1, Slice* s2);
+
+/// @brief Compare to buffers together and return a 1 if b1 > b2, return 0 if b1 == b2, and return -1 if b1 < b2
+/// @param b1 The first buffer
+/// @param b2 The second buffer
+i8 buffer_cmp(Buffer* b1, Buffer* b2);
+
+/// @brief Convert the given buffer into a Slice struct and return the pointer to that Slice
+/// @param buffer The buffer to convert into a slice 
+Slice* buffer_to_slice(Buffer* buffer);
+
+/// @brief Convert the given Slice into a Buffer and return the pointer to that Buffer
+/// @param slice The slice to convert into a buffer
+/// @param capacity The total capacity to allocate for the buffer
+Buffer* slice_to_buffer(Slice* slice, size_t capacity);
 
 #endif // STRING_SLICE_H
--- src/string_slice.c
+++ src/string_slice.c
@@ -1,41 +1,45 @@
-#include "string_slice.h"
+#include <stdlib.h>
+#include "../include/string_slice.h"
 
 /// @brief Converts a slice back into a standard C string
-/// @param slice The source StringSlice
-char* slice_to_string(const StringSlice slice) {
+/// @param slice The source Slice
+char* slice_to_string(const Slice slice) {
     return slice.begin;
 }
 
-/// @brief Converts a string to a StringSlice
+/// @brief Converts a string to a Slice
 /// @param string The source string
-StringSlice string_to_slice(const char* string) {
-    return (StringSlice){ .begin = (char*)string, .len = strlen(string)};
+Slice* string_to_slice(const char* string) {
+	Slice* slice = (Slice*)malloc(sizeof(size_t) + (sizeof(char) * strlen(string)));
+	memcpy(slice->begin, string, strlen(string) + 1);
+	slice->len = strlen(string);
+	return slice;
 }
 
-/// @brief Creates a StringSlice given a beginning offset and a total length.
+/// @brief Creates a Slice given a beginning offset and a total length.
 /// Will return an empty string (""\0) if offset is greater than the length of the source string.
 /// @param string The source string
 /// @param offset The offset of the start of the slice
 /// @param length The total length of the slice
-StringSlice slice_from_range(char* string, u64 offset, u64 length) {
+Slice* slice_from_range(char* string, u64 offset, u64 length) {
     // this code feels really dirty, pointer arithmetic is weird...
 
     if(offset > strlen(string)) {
         (string + strlen(string))[0] = '\0'; // need to truncate string so that string_to_slice() works properly
-        return (StringSlice){ .begin = (char*)(string + strlen(string)), .len = 0};
+        return (Slice){ .begin = (char*)(string + strlen(string)), .len = 0};
     }
     if(length <= strlen(string + offset)) {
         (string + offset)[length] = '\0'; // need to truncate string so that string_to_slice() works properly
-        return (StringSlice){ .begin = (char*)(string + offset), .len = length};
+        return (Slice){ .begin = (char*)(string + offset), .len = length};
     } else {
         u64 offsetted_string_length = strlen(string + offset);
-        return (StringSlice) { .begin = (char*)(string + offset), .len = offsetted_string_length};
+        return (Slice) { .begin = (char*)(string + offset), .len = offsetted_string_length};
     }
 }
 
 /// @brief Compares two string slices together and checks if they are equivalent or not
 /// @param str_1 The first slice
 /// @param str_2 The second slice
-bool streql(const StringSlice* str_1, const StringSlice* str_2) {
+bool streql(const Slice* str_1, const Slice* str_2) {
     return strcmp(str_1->begin, str_2->begin) == 0;
 }
